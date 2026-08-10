5d169b00ba66e2697d5030c6d9392c07d42188f9
MAHossain1 <mahpro110@gmail.com>
2026-08-10 07:51:07 +0600

    solved on docker compose

--- server-cmds.sh
+++ server-cmds.sh
@@ -1,9 +1,13 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
+
+set -e
 
 export IMAGE_NAME=arman04/java-maven-app:$1
 
-docker-compose -f docker-compose.yaml up --detach
+echo "Deploying $IMAGE_NAME"
+
+docker compose -f docker-compose.yaml pull
+
+docker compose -f docker-compose.yaml up -d
 
 echo "Docker image $IMAGE_NAME is deployed on EC2 instance"
-
-export TEST=testvalue
